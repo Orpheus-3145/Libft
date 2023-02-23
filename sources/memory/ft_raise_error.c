2aede8fb728e4181add7f9e85aed47b9ce8df656
Orpheus-3145 <francesco.aru25@gmail.com>
2023-02-23 22:48:37 +0100

    test

--- sources/memory/ft_raise_error.c
+++ sources/memory/ft_raise_error.c
@@ -6,7 +6,7 @@
 /*   By: fra <fra@student.codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/01/30 05:20:28 by fra           #+#    #+#                 */
-/*   Updated: 2023/02/23 22:37:55 by fra           ########   odam.nl         */
+/*   Updated: 2023/02/23 22:48:27 by fra           ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_raise_error(char *text, int end)
 {
-	ft_printf("henlo2!");
+	ft_printf("henlo3!");
 	ft_putstr_fd(text, 2);
 	ft_putchar_fd('\n', 2);
 	if (end)
