de9f411941edca45d50882e722a4f46b414d3e65
Orpheus-3145 <francesco.aru25@gmail.com>
2023-02-27 03:19:45 +0100

    fix final condition of ft_strncmp()

--- sources/strings/ft_strncmp.c
+++ sources/strings/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: faru <faru@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/05 11:19:15 by faru          #+#    #+#                 */
-/*   Updated: 2023/02/08 03:29:22 by fra           ########   odam.nl         */
+/*   Updated: 2023/02/27 03:19:03 by fra           ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	ft_strncmp(const char *str1, const char *str2, size_t n)
 			return (-1);
 		index++;
 	}
-	if (! str1[index] && ! str2[index] && index == n)
+	if (index == n || (! str1[index] && ! str2[index]))
 		return (0);
 	else
 		return (-1);
