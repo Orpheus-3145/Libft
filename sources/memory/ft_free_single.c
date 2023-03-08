6f79808f1cfd979078dbdbd5bf72b2e6f6cf8839
Orpheus-3145 <francesco.aru25@gmail.com>
2023-03-08 02:16:42 +0100

    tmp

--- sources/memory/ft_free_single.c
+++ sources/memory/ft_free_single.c
@@ -6,7 +6,7 @@
 /*   By: fra <fra@student.codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/23 18:32:15 by fra           #+#    #+#                 */
-/*   Updated: 2023/02/23 21:42:03 by fra           ########   odam.nl         */
+/*   Updated: 2023/03/08 02:16:21 by fra           ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_free_single(void **to_free)
 	if (to_free && *to_free)
 	{
 		free(*to_free);
-		to_free = NULL;
+		*to_free = NULL;
 	}
 	return (NULL);
 }
