b96c2691aa57503819c89bd92d669d18992cb245
Francesco Aru <faru@codam.student.nl>
2023-05-17 11:57:46 +0200

    update to ft_concat()

--- sources/strings/ft_concat.c
+++ sources/strings/ft_concat.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_concat.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: fra <fra@student.42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/16 19:17:30 by fra               #+#    #+#             */
-/*   Updated: 2023/05/16 20:20:12 by fra              ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   ft_concat.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: fra <fra@student.42.fr>                      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/05/16 19:17:30 by fra           #+#    #+#                 */
+/*   Updated: 2023/05/17 11:53:38 by faru          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,17 @@ char	*ft_concat(char *str1, char *str2)
 	len_s1 = ft_strlen(str1);
 	len_s2 = ft_strlen(str2);
 	cat = malloc((len_s1 + len_s2 + 1) * sizeof(char));
-	if (! cat)
-		return (NULL);
-	cat[len_s1 + len_s2] = '\0';
-	while (len_s2--)
-		cat[len_s1 + len_s2] = str2[len_s2];
-	while (len_s1--)
-		cat[len_s1] = str1[len_s1];
+	if (cat)
+	{
+		cat[len_s1 + len_s2] = '\0';
+		while (len_s2--)
+			cat[len_s1 + len_s2] = str2[len_s2];
+		while (len_s1--)
+			cat[len_s1] = str1[len_s1];
+	}
+	if (str1)
+		free(str1);
+	if (str2)
+		free(str2);
 	return (cat);
 }
