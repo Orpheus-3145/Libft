e65d3e2bde4f40c5d3d151a3f1c9f7d12ea35384
Francesco Aru <faru@codam.student.nl>
2023-05-24 19:15:04 +0200

    fixed that fucking ft_trim()

--- sources/strings/ft_trim.c
+++ sources/strings/ft_trim.c
@@ -6,7 +6,7 @@
 /*   By: faru <faru@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/17 12:42:47 by faru          #+#    #+#                 */
-/*   Updated: 2023/05/24 15:05:32 by faru          ########   odam.nl         */
+/*   Updated: 2023/05/24 15:55:15 by faru          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,20 @@
 char	*ft_trim(char *to_trim, bool free_str)
 {
 	char	*trimmed;
-	int32_t	start;
-	int32_t	end;
+	char	*tmp;
+	int32_t	length;
 
 	if ((! to_trim) || (! *to_trim))
 		return (to_trim);
-	start = 0;
-	end = ft_strlen(to_trim) - 1;
-	while (*to_trim && ft_isspace(*to_trim))
-	{
-		to_trim++;
-		end--;
-	}
-	while (ft_isspace(to_trim[end]))
-		end--;
-	if (end == (int32_t) ft_strlen(to_trim) - 1)
+	tmp = to_trim;
+	while (*tmp && ft_isspace(*tmp))
+		tmp++;
+	if (*tmp == '\0')
 		return (to_trim);
-	trimmed = ft_substr(to_trim, 0, end - start + 1);
+	length = ft_strlen(tmp) - 1;
+	while (ft_isspace(tmp[length]))
+		length--;
+	trimmed = ft_substr(tmp, 0, length + 1);
 	if (free_str)
 		free(to_trim);
 	return (trimmed);
