b66221e8fb04fc6270e2a3e2d65be1550915f460
fra <francesco.aru25@gmail.com>
2023-07-21 23:12:00 +0200

    tmp

--- sources/numbers/ft_pow.c
+++ sources/numbers/ft_pow.c
@@ -6,15 +6,15 @@
 /*   By: fra <fra@student.42.fr>                      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/01/16 20:45:19 by fra           #+#    #+#                 */
-/*   Updated: 2023/03/05 01:43:52 by fra           ########   odam.nl         */
+/*   Updated: 2023/07/21 23:11:47 by fra           ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-float	ft_pow(int base, int exp)
+double	ft_pow(int base, int exp)
 {
-	float	pow;
+	double	pow;
 
 	pow = 1;
 	if (exp > 0)
@@ -23,6 +23,6 @@ float	ft_pow(int base, int exp)
 			pow *= base;
 	}
 	else
-		pow = (float) 1 / ft_pow(base, exp * -1);
+		pow = (double) 1 / ft_pow(base, exp * -1);
 	return (pow);
 }
